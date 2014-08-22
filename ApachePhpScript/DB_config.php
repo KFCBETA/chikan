400fc15c88497db54d533b4605c0b5bf20fab8e0
Heiru Wu <wuheiru.5203@gmail.com>
2014-08-22 10:49:03 +0800

    delete DB_config

--- ApachePhpScript/DB_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    global $_DB;	
-    $_DB['host'] = "127.0.0.1:5566";
-    $_DB['username'] = "chikan_develop";
-    $_DB['password'] = "heirujames";
-    $_DB['dbname'] = "chikanDataBase";
-?>
